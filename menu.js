1a9ef4fe7a2d50a26dbb7384ba296d33418a817d
Juan Villa Crisosto <118411109+JuVillaC@users.noreply.github.com>
2026-01-16 18:51:41 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -25,66 +25,11 @@ $(document).ready(function () {
         window.location.href = 'index.html';
     });
 
-    // Abrir menú lateral
-    $('#openMenu').on('click', function () {
-    $('#sideMenu').addClass('active');
+    $('#menuLogout').on('click', function () {
+        localStorage.removeItem('logged');
+        window.location.href = 'index.html';
     });
 
-    // Cerrar menú lateral
-    $('#closeMenu').on('click', function () {
-    $('#sideMenu').removeClass('active');
-    });
-
-    // Acciones del menú
-    $('#menuDeposit').click(() => window.location.href = 'deposit.html');
-    $('#menuSend').click(() => window.location.href = 'sendmoney.html');
-    $('#menuMov').click(() => window.location.href = 'transactions.html');
-    $('#menuLogout').click(() => {
-    localStorage.removeItem('logged');
-    window.location.href = 'index.html';
-    });
-
-    // Abrir notificaciones
-    $('#openNotifications').on('click', function () {
-        $('#notifications').addClass('active');
-    });
-
-    // Cerrar notificaciones
-    $('#closeNotifications').on('click', function () {
-        $('#notifications').removeClass('active');
-    });
-
-    // Abrir menú lateral
-    $('#openMenu').on('click', function () {
-        $('#sideMenu').addClass('active');
-        $(this).addClass('icon-active');
-    });
-
-    // Cerrar menú lateral
-    $('#closeMenu').on('click', function () {
-        $('#sideMenu').removeClass('active');
-        $('#openMenu').removeClass('icon-active');
-    });
-
-    // Abrir notificaciones
-    $('#openNotifications').on('click', function () {
-        $('#notifications').addClass('active');
-        $(this).addClass('bell-active');
-    });
-
-    // Cerrar notificaciones
-    $('#closeNotifications').on('click', function () {
-        $('#notifications').removeClass('active');
-        $('#openNotifications').removeClass('bell-active');
-    });
-
-    function abrirOverlay() {
-        $('#overlay').addClass('active');
-    }
-
-    function cerrarOverlay() {
-        $('#overlay').removeClass('active');
-    }
 
     // Menú
     $('#openMenu').on('click', function () {
@@ -116,5 +61,7 @@ $(document).ready(function () {
         cerrarOverlay();
     });
 
+});
 
 });
+
